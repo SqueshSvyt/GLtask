64c1fac7acbd8ef2f01ba43bfa1f4ff466fc4101
SqueshSvyt <svytsop@gmail.com>
2023-08-03 16:29:18 +0300

    Add interface to work with prog

--- Program/main.cpp
+++ Program/main.cpp
@@ -6,22 +6,43 @@ int main() {
     std::string searchPath = R"(C:\)";
 
     //Program Interface basic
-    std::string fileName;
-    std::cout << "Enter file name: ";
-    std::cin >> fileName;
-    std::cout << "Searching for file '" << fileName << "'..." << '\n';
+    while (true) {
+        std::cout << "Select an option:" << std::endl;
+        std::cout << "1. Run Search" << std::endl;
+        std::cout << "2. Exit the Program" << std::endl;
+        std::string fileName;
+        std::string Result_path;
 
-    //Searching
-    std::string Result_path;
-    searchFile(fileName, searchPath, Result_path);
+        int choice;
+        std::cin >> choice;
 
-    //Output result and end
-    if (!Result_path.empty()) {
-        std::cout << '\n' << "File '" << fileName << "' find file by way: " << Result_path << std::endl;
-    } else {
-        std::cout << '\n' << "File '" << fileName << "' not found." << std::endl;
+        switch (choice) {
+            case 1:
+                fileName = "";
+                std::cout << "Enter file name: ";
+                std::cin >> fileName;
+                std::cout << "Searching for file '" << fileName << "'..." << '\n';
+
+                //Searching
+                Result_path = "";
+                searchFile(fileName, searchPath, Result_path);
+
+
+                //Output result and end
+                if (!Result_path.empty()) {
+                    std::cout << '\n' << "File '" << fileName << "' find file by way: " << Result_path << std::endl;
+                } else {
+                    std::cout << '\n' << "File '" << fileName << "' not found." << std::endl;
+                }
+
+
+                system("pause");
+                break;
+            case 2:
+                std::cout << "Thank you for using the program. Goodbye!" << std::endl;
+                return 0; // Exit the program
+            default:
+                std::cout << "Invalid choice. Please try again." << std::endl;
+        }
     }
-
-    system("pause");
-    return 0;
 }
